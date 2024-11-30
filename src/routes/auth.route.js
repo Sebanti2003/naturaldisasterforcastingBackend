3697143a4e0650372de33084a429409e564a019c
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-11-30 22:24:09 +0530

    forgot and reset password

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { login, signup, veerificationtokencheck } from "../controllers/auth.controller.js";
+import {
+    forgotpasswordemailsent,
+  login,
+  logout,
+  resetPassword,
+  signup,
+  veerificationtokencheck,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.route("/signup").post(signup);
 router.route("/login").post(login);
-router.route('/verify-email/:userid').get(veerificationtokencheck);
+router.route("/logout").get(logout);
+router.route("/verify-email/:userid").get(veerificationtokencheck);
+router.route("/forgot-password").post(forgotpasswordemailsent);
+router.route("/reset-password/:userid").post(resetPassword);
 
 export default router;
