168dddc2a1f3a574c5d95e2949e0543eacc91c07
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-12-01 12:34:16 +0530

    done the home route

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 import testrouter from "./routes/test.route.js";
 import authrouter from "./routes/auth.route.js";
-import userrouter from './routes/user.route.js'
+import userrouter from "./routes/user.route.js";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 const app = express();
@@ -15,15 +15,17 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 //route imports
-
+app.use("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to Natural Disaster Forecasting Backend",
+  });
+});
 app.use("/api/v1/user/test", testrouter);
 app.use("/api/v1/user/auth", authrouter);
-app.use("/api/v1/user",userrouter);
-
+app.use("/api/v1/user", userrouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
-
 
 export default app;
