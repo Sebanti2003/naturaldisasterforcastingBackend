a689894b4e58073f0caf498f8b598cde93d5a48d
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-12-12 22:08:03 +0530

    added the frontend url

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: "https://forecast-frontend-iwid.vercel.app/",
     credentials: true,
   })
 );
